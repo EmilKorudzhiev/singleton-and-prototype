f646c4e1fe25b9ddf078760f9b9a28ee3a7e3034
Emil Korudzhiev <e.korudzhiev@abv.bg>
2023-11-29 11:15:15 +0200

    Initial commit

---
+++ src/main/java/dev/emilkorudzhiev/singletonandprototype/SingletonAndPrototypeApplication.java
@@ -0,0 +1,13 @@
+package dev.emilkorudzhiev.singletonandprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingletonAndPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingletonAndPrototypeApplication.class, args);
+    }
+
+}
